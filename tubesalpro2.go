b1664af5d74b0b6d8ea130f44e2ee1712911a5c8
mutiarnfz <mutiara.nafiza@gmail.com>
2025-05-22 13:41:52 +0700

    Update tubesalpro2.go

--- tubesalpro2.go
+++ tubesalpro2.go
@@ -121,11 +121,11 @@ func main() {
 
 				fmt.Println("Berikut seluruh data pekerjaan yang sesuai dengan range gaji yang anda inginkan!")
 				fmt.Printf("| %-2s | %-30s | %-12s |\n", "No", "Pekerjaan", "Gaji")
-				fmt.Println("------------------------------------------------------")
+				fmt.Println("______________________________________________________")
 				for i = 0; i < N; i++ {
 					fmt.Printf("| %-2d | %-30s | %-12d |\n", i+1, dataPekerjaan[i].title, dataPekerjaan[i].gaji)
 				}
-				fmt.Println("------------------------------------------------------")
+				fmt.Println("______________________________________________________")
 			}
 
 		case 5:
